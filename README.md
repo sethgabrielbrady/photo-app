e96d3078a3e4db9dbce10c61794c5730c2b7135b
Seth Brady <sethgabrielbrady@gmail.com>
2019-05-23 09:30:57 -0400

    fixed devise-bootstrap issue; updated sendgrid settings and added the api key to the heroku config

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ https://sethgb-photo-app.herokuapp.com/
 
 
 TODO:
-"*= require devise_bootstrap_views" - figure out why this is causing issues in application.css
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,10 +89,19 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.delivery_method = :test
-
-  config.action_mailer.default_url_options = { :host => 'sethgb-photo-app.herokuapp.com/', :protocol => 'https'}
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'sethgb-photo-app.herokuapp.com/', :protocol => 'https'}
 end
